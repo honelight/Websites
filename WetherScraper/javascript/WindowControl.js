a5dd221084dc2a7a54afe4dedf50937738db3565
Hongyao Shi <honelight@gmail.com>
2015-07-29 20:26:52 -0500

    weather scraper added

---
+++ WetherScraper/javascript/WindowControl.js
@@ -0,0 +1,17 @@
+﻿$("#wrapper").css("min-height", $(window).height())
+
+$("#weatherPanel").submit(function (e) {
+    //console.log("here");
+    e.preventDefault();
+    if ($("#searchBox").val() == "") {
+        $("#weatherReport").css("display", "none");
+        $("#emptyMessage").css("display", "block");
+    }
+    else {
+        $("#emptyMessage").css("display", "none");
+        $.get("./php/Scrapper.php?city=" + $("#searchBox").val(), function (data) {
+            $("#weatherReport").html(data);
+            $("#weatherReport").css("display", "block");
+        });
+    }
+});
